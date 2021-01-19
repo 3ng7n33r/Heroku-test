4083d898cc19c9172035c89a0f1bd9fa1360dcd9
Max Blacher-L <m.blacher@web.de>
2021-01-19 11:01:37 +0100

    Initial commit

---
+++ translation/urls.py
@@ -0,0 +1,21 @@
+from django.urls import path
+
+from . import views
+
+urlpatterns = [
+    path('', views.index, name='index'),
+    path('<str:base_language>/<str:base_flag>', views.index, name='index'),
+    # ex: /de/
+    path('<str:base_language>/<str:base_flag>/<str:target_language>/<str:target_flag>',
+         views.translation, name='translation'),
+    path('<str:base_language>/<str:base_flag>/<str:target_language>/<str:target_flag>/documents',
+         views.documents, name='documents'),
+    path('<str:base_language>/<str:base_flag>/documents',
+         views.docindex, name='docindex'),
+    path('support', views.support, name='support'),
+    path('<str:base_language>/<str:base_flag>/support',
+         views.support, name='support'),
+    path('about', views.about, name='about'),
+    path('<str:base_language>/<str:base_flag>/about',
+         views.about, name='about'),
+]
